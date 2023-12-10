23a2e5abdb46adcbabd2222613f8257512ed48a0
devSudhir <sudhirsamantaray33@gmail.com>
2023-12-10 17:25:28 +0530

    async-callback-settimeout-setinterval-promise

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,20 @@
 */
 
 function wait(n) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("Promise resolved");
+    }, n * 1000);
+  });
 }
+
+const result = wait(6);
+result.then(function (data) {
+  console.log(data);
+});
+
+async function promiseData() {
+  console.log("Await", await wait(3));
+}
+
+promiseData();
